0ebbdb2bd14af98382261854bf32b04534dc034c
Michael Bianco <iloveitaly@gmail.com>
2022-12-28 15:26:48 -0700

    fix bad sha

--- Formula/hyper-focus.rb
+++ Formula/hyper-focus.rb
@@ -2,7 +2,7 @@ class HyperFocus < Formula
   desc "Block distracting websites and applications"
   homepage "https://github.com/iloveitaly/hyper-focus"
   url "https://github.com/iloveitaly/hyper-focus/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "376ec7522ad4889a4144ad8cb434141e84ca3caec9b67914c72a2e7737abd373"
+  sha256 "2f7b182e09873d285f95da39264464f108abf13dfb5c8174111a7dc8979b848c"
   license "MIT"
 
   service do
