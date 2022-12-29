9714a04d5d64b402683a1fb087aa4ad461f890d9
Michael Bianco <iloveitaly@gmail.com>
2022-12-29 09:27:36 -0700

    update hyper-focus formula

--- Formula/hyper-focus.rb
+++ Formula/hyper-focus.rb
@@ -1,9 +1,11 @@
 class HyperFocus < Formula
   desc "Block distracting websites and applications"
   homepage "https://github.com/iloveitaly/hyper-focus"
-  url "https://github.com/iloveitaly/hyper-focus/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "2f7b182e09873d285f95da39264464f108abf13dfb5c8174111a7dc8979b848c"
+  url "https://github.com/iloveitaly/hyper-focus/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "3ea31094b71485659522f6aee4486a15baacf6b846fbecda9a6dca1735dd0449"
   license "MIT"
+
+  depends_on :macos
 
   service do
     run [opt_bin/"hyper-focus"]
